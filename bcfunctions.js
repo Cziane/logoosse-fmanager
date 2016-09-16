8bb1c11a11853df41fc5df65e7b89ac6f2ddb01a
Clement Ziane <logoosse@cziane.net>
2016-09-16 15:19:48 +0200

    add files functions client and server and methode

---
+++ bcfunctions.js
@@ -0,0 +1,12 @@
+function storeFile(file, public=false){
+	var myFile={
+		header:{
+			name:file.name,
+			type:file.type,
+			size: file.size,
+		},
+		content:{
+
+		}
+	}
+}
--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'logoosse:blobs-collections',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Server pacakge to store blops in mongo from db',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Logoosse/logoosse-blobs-collections.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
