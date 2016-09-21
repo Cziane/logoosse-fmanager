2ec4961bfdf0dc2840e7025de7bbf7233edbd056
Clement Ziane <logoosse@cziane.net>
2016-09-21 10:26:13 +0200

    modify package name and add dependencies : -restivus -collections2 -coffeescript

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'logoosse:blobs-collections',
+  name: 'logoosse:fmanager',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Server pacakge to store blops in mongo from db',
+  summary: 'Server pacakge to store files in meteor app with mongodb or filemanger',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/Logoosse/logoosse-blobs-collections.git',
+  git: 'https://github.com/Logoosse/logoosse-fmanager.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,12 +12,13 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.1.1');
-  api.use('ecmascript');
-  api.mainModule('blobs-collections.js');
+  api.use('nimble:restivus');
+  api.use('aldeed:collection2');
+  api.use('coffeescript')
+
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
   api.use('tinytest');
   api.use('logoosse:blobs-collections');
   api.mainModule('blobs-collections-tests.js');
